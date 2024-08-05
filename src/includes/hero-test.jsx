e479f0c05723118925a05cffa125f3f579ea3753
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2024-08-05 09:10:23 +0300

    save

---
+++ src/includes/hero-test.jsx
@@ -0,0 +1,12 @@
+import { Button } from "flowbite-react";
+import * as React from 'react';
+
+export default function Component() {
+  return (
+    <Button.Group>
+      <Button color="gray">Profile1</Button>
+      <Button color="gray">Settings</Button>
+      <Button color="gray">Messages</Button>
+    </Button.Group>
+  );
+}
