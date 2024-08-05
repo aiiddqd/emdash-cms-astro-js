07a77691483a070de144c6614b31970ffb5e750a
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2024-08-05 09:57:04 +0300

    q

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './app.css'
 import { Button, Avatar } from "flowbite-react";
 import Hero from "./includes/hero-test";
