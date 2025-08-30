850c1e83879e54cebaf09523a2b0c7c54263f841
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-08-30 14:23:04 +0300

    Implement Notion API integration with methods for adding comments and changing page status

--- includes/NotionService.php
+++ includes/NotionService.php
@@ -3,21 +3,76 @@
 namespace ProcessFlows;
 
 use Notion\Databases\Properties\StatusOption;
+use Notion\Databases\Properties\Status;
+
 use Notion\Notion;
 use Notion\Databases\Query;
 use Notion\Databases\Query\CompoundFilter;
 use Notion\Databases\Query\DateFilter;
 use Notion\Databases\Query\Sort;
 use Notion\Databases\Query\StatusFilter;
+use Notion\Comments\Comment;
+use Notion\Common\RichText;
 
 NotionService::init();
 class NotionService
 {
+    public static string $token = '';
+    public static Notion $notion;
+
     public static function init()
     {
         // add settings to Notion API integration Plugin::$settings_slug
 
         add_action('admin_init', [self::class, 'add_settings']);
+    }
+
+    public function __construct($token = null)
+    {
+        if ($token) {
+            self::$token = $token;
+        } else {
+            self::$token = Plugin::getConfig('notion_api_key');
+        }
+
+        // if empty self::$token
+        if (empty(self::$token)) {
+            throw new \Exception('Notion API token is not set.');
+        }
+
+        self::$notion = Notion::create(self::$token);
+    }
+
+    public static function addCommentToPage($pageId, $comment)
+    {
+        // Create the comment content as rich text
+        $richText = RichText::fromString($comment);
+
+        // Create a Comment object
+        $comment = Comment::create($pageId, $richText);
+
+        // Add the comment to the page
+        return self::$notion->comments()->create($comment);
+    }
+
+    public static function changeNotionPageStatus($pageId, $newStatus)
+    {
+        $page = self::$notion->pages()->find($pageId);
+        if (! $page) {
+            return false;
+        }
+
+        /**
+         * @var Status $status
+         */
+        $status = $page->getProperty('Status');
+        if ($status) {
+            $page = $page->addProperty("Status", $status->changeOption(StatusOption::fromName($newStatus)));
+            self::$notion->pages()->update($page);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static function add_settings()
